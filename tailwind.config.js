2f39df76e80ad82a190336dab4be2b5eda0a1852
Tess99854 <stesnime@yahoo.com>
2022-06-04 23:11:15 +0100

    setup layout and add prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./layouts/**/*.{js,jsx,ts,tsx}",
   ],
+  theme: {
+    extend: {
+      colors: {
+        "dark-back": "#111111",
+        green: "#02DC85",
+        blue: "#4E44CE",
+      },
+    },
+  },
   plugins: [require("daisyui")],
-}
+};
