73758c5d54988e67c8db067a5155df35e23e2178
Abdelhakeem <abdelhakeem.osama@hotmail.com>
2022-06-04 22:34:07 +0200

    Create empty pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Seagull</title>
+        <meta name="description"
+              content="Send custom transactions to Solana" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
---
+++ pages/main.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next'
+
+const Main: NextPage = () => {
+  return <h1>Main Page</h1>
+}
+
+export default Main
