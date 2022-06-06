33fde5976df27a66060790b00e558520900bf169
Abdelhakeem <abdelhakeem.osama@hotmail.com>
2022-06-06 18:38:15 +0200

    Add Default preset

--- components/PresetTransactionPicker.tsx
+++ components/PresetTransactionPicker.tsx
@@ -10,6 +10,9 @@ function PresetTransactionPicker({onChange}: Props) {
   return (
     <div className="flex flex-col gap-3 font-nunito text-white text-xl">
       <label className="text-white">Pick a preset transaction:</label>
+      <button className="btn" onClick={() => onChange('default')}>
+        Default
+      </button>
       <button className="btn" onClick={() => onChange('transfer')}>
         Transfer
       </button>
